19396909db71ae7bffacbc4f99de8c8d3d2b4307
Panjie Setiawan Wicaksono <panjie@panjiesw.com>
2025-08-30 20:45:29 +0700

    feat: :lock: page protection

--- app/jobs/[id]/edit/page.tsx
+++ app/jobs/[id]/edit/page.tsx
@@ -2,12 +2,17 @@ import { DefaultLayout } from '@/components/default-layout';
 import { JobForm } from '@/components/job-form';
 import { PARAM_ID, TABLE_JOB } from '@/lib/constants';
 import { createClient } from '@/lib/supabase/server';
-import { notFound } from 'next/navigation';
+import { notFound, redirect } from 'next/navigation';
 
 export default async function EditJob(props: PageProps<'/jobs/[id]/edit'>) {
   const supabase = await createClient();
 
   const { id } = await props.params;
+
+  const { data: userData, error } = await supabase.auth.getUser();
+  if (error || !userData?.user) {
+    return redirect(`/auth/login?redirect_to=/jobs/${id}/edit`);
+  }
   const { data } = await supabase
     .from(TABLE_JOB)
     .select('*,user_profile(*)')
@@ -15,12 +20,13 @@ export default async function EditJob(props: PageProps<'/jobs/[id]/edit'>) {
     .limit(1)
     .single();
 
-  if (data == null) {
+  if (data == null || data.user_profile.id !== userData.user.id) {
     return notFound();
   }
   return (
     <DefaultLayout>
       <main className="flex-1 px-4 min-w-2xl">
+        <h2 className="font-semibold text-xl mb-4">Edit Job Post</h2>
         <JobForm id={id} defaultValues={{ ...data }} />
       </main>
     </DefaultLayout>
--- app/jobs/new/page.tsx
+++ app/jobs/new/page.tsx
@@ -1,10 +1,20 @@
 import { DefaultLayout } from '@/components/default-layout';
 import { JobForm } from '@/components/job-form';
+import { createClient } from '@/lib/supabase/server';
+import { redirect } from 'next/navigation';
 
-export default function NewJob() {
+export default async function NewJob() {
+  const supabase = await createClient();
+
+  const { data, error } = await supabase.auth.getUser();
+  if (error || !data?.user) {
+    return redirect("/auth/login?redirect_to=/jobs/new");
+  }
+
   return (
     <DefaultLayout>
       <main className="flex-1 px-4 min-w-2xl">
+        <h2 className="font-semibold text-xl mb-4">Add New Job Post</h2>
         <JobForm />
       </main>
     </DefaultLayout>
