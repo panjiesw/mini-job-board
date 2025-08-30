6636d5a381853d7ca731c329b548377d6fbdf9c5
Panjie Setiawan Wicaksono <panjie@panjiesw.com>
2025-08-30 18:45:47 +0700

    fix: :bug: wrap `JobList` with `Suspense`

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,15 @@
 import { DefaultLayout } from '@/components/default-layout';
 import { JobList } from '@/components/job-list';
+import { Suspense } from 'react';
 
 export default function Home() {
   return (
     <DefaultLayout>
       <main className="flex-1 flex flex-col gap-6 px-4 w-full">
         <h2 className="font-semibold text-xl mb-4">Available Jobs</h2>
-        <JobList />
+        <Suspense>
+          <JobList />
+        </Suspense>
       </main>
     </DefaultLayout>
   );
